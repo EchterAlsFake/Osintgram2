6b604afbeaf9877ef24bc71adff23881844ac747
Johannes Habel <EchterAlsFake@proton.me>
2023-06-24 13:12:11 +0200

    k

--- Osintgram.py
+++ Osintgram.py
@@ -5,7 +5,7 @@ import wget
 from instagrapi import Client
 from colorama import *
 from tqdm import tqdm
-from ui_form import Ui_Inst_downloader_widget
+from ui_form import Ui_Form
 from PySide6.QtWidgets import QApplication, QWidget
 from PySide6.QtCore import QThread, Signal
 import os
@@ -91,85 +91,8 @@ class Widget(QWidget):
         super().__init__(parent)
         self.login_worker = None
         self.z = Fore.LIGHTGREEN_EX + "[+]"
-        self.ui = Ui_Inst_downloader_widget()
+        self.ui = Ui_Form()
         self.ui.setupUi(self)
-        self.ui.push_buton_download_photos.setDisabled(True)
-        self.ui.push_button_download_propic.setDisabled(True)
-        self.ui.push_button_submit.setDisabled(True)
-        self.ui.push_button_login.clicked.connect(self.login)
-        self.ui.push_button_submit.clicked.connect(self.get_profile_info)
-        self.ui.push_buton_download_photos.clicked.connect(self.download_images)
-        self.ui.push_button_download_propic.clicked.connect(self.download_propic)
-        self.cl = Client()
-        if os.path.isdir("output"):
-            pass
-
-        elif not os.path.isdir("output"):
-            os.mkdir("output")
-
-
-    def login(self):
-        email = self.ui.line_edit_email.text()
-        password = self.ui.line_edit_password.text()
-
-        # Create worker
-        self.login_worker = LoginWorker(email, password)
-        # Connect worker signal to slot
-        self.login_worker.status_signal.connect(self.update_login_status)
-        # Start worker
-        self.login_worker.start()
-
-    def update_login_status(self, progress, status):
-        self.ui.prgressbar_login.setValue(progress)
-        print(status)
-        if progress == 100:
-            self.ui.line_edit_target.setDisabled(False)
-            self.ui.line_edit_target.clear()
-            self.ui.push_button_submit.setDisabled(False)
-
-    def get_profile_info(self):
-        self.target_name = self.ui.line_edit_target.text()
-        self.target_id = self.cl.user_id_from_username(self.target_name)
-        self.ui.line_edit_id.setText(str(self.target_id))
-
-        # Create worker
-        self.profile_info_worker = ProfileInfoWorker(self.cl, self.target_name)
-        # Connect worker signal to slot
-        self.profile_info_worker.info_signal.connect(self.update_profile_info)
-        # Start worker
-        self.profile_info_worker.start()
-
-    def update_profile_info(self, data, images):
-        self.data = data
-        self.images = images
-        # Update UI with data...
-        # ...
-        self.ui.line_edit_full_name.setText(str(self.data['full_name']))
-        self.ui.line_edit_verified.setText(str(self.data['is_verified']))
-        self.ui.line_edit_follower_count.setText(str(self.data['follower_count']))
-        self.ui.line_edit_following_count.setText(str(self.data['following_count']))
-        self.ui.line_edit_total_images.setText(str(len(self.images)))
-        self.ui.progressbar_photos.setMaximum(len(self.images))
-        self.ui.push_buton_download_photos.setDisabled(False)
-        self.ui.push_button_download_propic.setDisabled(False)
-
-    def download_images(self):
-        # Create worker
-        self.download_images_worker = DownloadImagesWorker(self.images)
-        # Connect worker signal to slot
-        self.download_images_worker.progress_signal.connect(self.update_download_progress)
-        # Start worker
-        self.download_images_worker.start()
-
-    def update_download_progress(self, progress):
-        self.ui.progressbar_photos.setValue(progress)
-
-    def download_propic(self):
-
-        self.ui.progressbar_propic.setValue(0)
-        url = self.data['profile_pic_url_hd']
-        wget.download(url, out="output/")
-        self.ui.progressbar_propic.setValue(100)
 
 
 

0c470cfea90ebfc014082e9eb95bdc8dab1d99f2
EchterAlsFake <EchterAlsFake@proton.me>
2023-09-16 19:10:01 +0200

    Testing install script...

---
+++ install.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+# Function to check if a command exists
+command_exists () {
+  type "$1" &> /dev/null ;
+}
+
+# Determine Linux Distribution
+distro=""
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    distro=$ID
+elif [ "$(uname -o)" == "Android" ]; then
+    distro="termux"
+fi
+
+# 1) Check and install Python3 and pip
+if ! command_exists python3; then
+    echo "Installing Python3..."
+    case $distro in
+        "termux") pkg install python ;;
+        "arch") sudo pacman -S python ;;
+        "fedora") sudo dnf install python3 ;;
+        "opensuse") sudo zypper install python3 ;;
+        "debian"|"ubuntu"|"kali"|"parrot") sudo apt update && sudo apt install -y python3 ;;
+        *) echo "Sorry, this distro is not supported" ;;
+    esac
+fi
+
+if ! command_exists pip; then
+    echo "Installing pip..."
+    case $distro in
+        "termux") pkg install python ;;
+        "arch") sudo pacman -S python-pip ;;
+        "fedora") sudo dnf install python3-pip ;;
+        "opensuse") sudo zypper install python3-pip ;;
+        "debian"|"ubuntu"|"kali"|"parrot") sudo apt update && sudo apt install -y python3-pip ;;
+        *) echo "Sorry, this distro is not supported" ;;
+    esac
+fi
+
+# 2) Check and install Git
+if ! command_exists git; then
+    echo "Installing Git..."
+    case $distro in
+        "termux") pkg install git ;;
+        "arch") sudo pacman -S git ;;
+        "fedora") sudo dnf install git ;;
+        "opensuse") sudo zypper install git ;;
+        "debian"|"ubuntu"|"kali"|"parrot") sudo apt update && sudo apt install -y git ;;
+        *) echo "Sorry, this distro is not supported" ;;
+    esac
+fi
+
+# 3) Termux specific installation
+if [ "$distro" == "termux" ]; then
+    pkg install libjpeg-turbo
+fi
+
+# 4) Clone the repository
+git clone https://github.com/EchterAlsFake/Osintgram2
+
+# 5) cd into it and run pip install
+cd Osintgram2 || { echo "Failed to change directory to Osintgram2"; exit 1; }
+pip install -r requirements.txt
+
+# 6) Run pyinstaller
+pyinstaller -F Osintgram.py
+
+# 7) chmod +x to the Osintgram file
+cd dist || { echo "Failed to change directory to dist"; exit 1; }
+chmod +x Osintgram
+
+echo "Done"
